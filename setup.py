1f050d511b67a27639e495c24c38debc75a36674
Alex Khomchenko <akhomchenko@gmail.com>
2018-05-12 15:08:23 +0200

    update setup.py to follow format expected by Lektor

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup
 
 setup(
     name='lektor-yandex-metrica',
-    version='0.1.1',
+    version='0.1.2',
     author='Alex Khomchenko',
     author_email='akhomchenko@gmail.com',
     url='https://github.com/gagoman/lektor-yandex-metrica',
     license='MIT',
     description='Adds support for Yandex Metrica to Lektor CMS',
+    keywords='Lektor plugin',
     long_description=open('README.rst').read(),
     py_modules=['lektor_yandex_metrica'],
     entry_points={
@@ -20,6 +21,8 @@ setup(
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3',
+        'Framework :: Lektor',
+        'Environment :: Plugins'
     ]
 )
